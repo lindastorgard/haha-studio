9f0fa8cfd2e67ac9c0c96eb4f873bb7f0f1a45f2
Viktorija Valso <viktorija@dyna.mo>
2020-05-27 23:48:21 +0200

    add relation to landing page model,

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,37 +1,69 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import PropTypes from 'prop-types'
+import PropTypes, { array } from 'prop-types'
+import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 import { Link, graphql } from 'gatsby'
-import Img from 'gatsby-image'
 import Layout from '../components/Layout'
 
-export const IndexPageTemplate = ({
-  image,
-  alt,
-  title,
-  heading,
-  // section,
-}) => (
-  <div>
-    <h1>{title}</h1>
-    <h3 >{heading}</h3>
+export const IndexPageTemplate = ({ title, image, imageAlt , section}) => {
+  return (
     <section>
-      <Link  to="/products">See all products</Link>
-      {/* <Img fluid={image.childImageSharp.fluid} alt={alt} /> */}
+      <h2>
+          {title}
+        </h2>
+       {image ? (
+          <PreviewCompatibleImage
+            imageInfo={{
+              image: image,
+              alt: imageAlt,
+            }}
+          />  
+        ) : null}
+       {section.map( s => {
+         return(
+           <section key={s.heading} sx={{display: "flex"}}>
+              <div sx={{flex: 3}}>
+                <PreviewCompatibleImage
+                  imageInfo={{
+                    image: s.image,
+                    alt: s.alt,
+                  }}
+               />  
+              </div>
+             <article sx={{flex: 2}}>
+              <h2>{s.heading}</h2>
+              <p>{s.text}</p>
+              <button>{s.linktitle}</button>
+              </article>
+           </section>
+         )
+       })}
     </section>
-  </div>
-)
+  )
+}
+
+
+IndexPageTemplate.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  imageAlt: PropTypes.string,
+  heading: PropTypes.string,
+  section: PropTypes.array,
+  // intro: PropTypes.shape({
+  //   blurbs: PropTypes.array,
+  // }),
+}
 
 const IndexPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark
-
   return (
     <Layout>
       <IndexPageTemplate
-        image={frontmatter.image}
         title={frontmatter.title}
+        image={frontmatter.image}
+        imageAlt={frontmatter.alt}
         heading={frontmatter.heading}
-        // section={frontmatter.section}
+        section={frontmatter.section}
       />
     </Layout>
   )
@@ -47,12 +79,13 @@ IndexPage.propTypes = {
 
 export default IndexPage
 
+
 export const pageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      id
       frontmatter {
         title
-        heading
         image {
           childImageSharp {
             fluid(maxWidth: 2048, quality: 100) {
@@ -60,33 +93,34 @@ export const pageQuery = graphql`
             }
           }
         }
+        alt
+        heading
+        section {
+          heading
+          text
+          linktitle
+          alt
+          image {
+            childImageSharp {
+              fluid(maxWidth: 2048, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
       }
     }
   }
 `
-IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-}
 
 
-// section {
-//   heading
-//   text
-//   linktitle
-//   alt
-//   image {
-//     childImageSharp {
-//       fluid(maxWidth: 240, quality: 64) {
-//         ...GatsbyImageSharpFluid
-//       }
-//     }
-//   }
-// }
+
+
+
+
+
+
+
+
+
+
--- src/templates/product-post.js
+++ src/templates/product-post.js
@@ -11,14 +11,7 @@ import ImageGallery from "../components/ImageGallery"
 
 
 export const ProductPostTemplate = ({ 
-//   description, 
-//   details, 
-//   featuredimage, 
-//   featuredimagealt, 
-//   photocredits, 
-//   releaseyear, 
-//   title, 
-//   imagegallery,
+  title, 
   helmet 
 }) => {
    
@@ -26,22 +19,8 @@ export const ProductPostTemplate = ({
       <SectionContainer>
         {helmet || ''}
         <section sx={{display: "flex"}}>
-        {featuredimage ? (
-          <div sx={{flex: 3}}>
-            <PreviewCompatibleImage
-              imageInfo={{
-                image: featuredimage,
-                alt: featuredimagealt,
-              }}
-            /> 
-            <p>{photocredits}</p>
-          </div>
-        ) : null}
         <article sx={{flex: 2}}>
           <h1>{title}</h1>
-          <p>{details}</p>
-          <p>{releaseyear}</p>
-          <p>{description}</p>
         </article>
 
         </section>
@@ -53,18 +32,12 @@ export const ProductPostTemplate = ({
     )
 };
 
-const ProjectPost = ({ data }) => {
+const ProductPost = ({ data }) => {
   const { markdownRemark: post } = data
   return (
     <Layout>
-      <ProjectPostTemplate
+      <ProductPostTemplate
         description={post.frontmatter.description}
-        // details={post.frontmatter.details}
-        // featuredimage={post.frontmatter.featuredimage}
-        // featuredimagealt={post.frontmatter.featuredimagealt}
-        // photocredits={post.frontmatter.photocredits}
-        // releaseyear={post.frontmatter.releaseyear}
-        // imagegallery={post.frontmatter.imagegallery}
         helmet={
           <Helmet titleTemplate="%s | Product">
             <title>{`${post.frontmatter.title}`}</title>
@@ -80,16 +53,16 @@ const ProjectPost = ({ data }) => {
   )
 }
 
-ProjectPost.propTypes = {
+ProductPost.propTypes = {
   data: PropTypes.shape({
     markdownRemark: PropTypes.object,
   }),
 }
 
-export default ProjectPost
+export default ProductPost
 
 export const pageQuery = graphql`
-  query ProjductPostByID($id: String!) {
+  query ProductPostByID($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
       frontmatter {
